5dd4697556e716f0eb8e1242308cf6100db859e8
Gordon Murray <gordon@gordonmurray.com>
2022-04-05 21:16:42 +0100

    kinesis firehose delivery stream

---
+++ kinesis.tf
@@ -0,0 +1,24 @@
+# Kinesis firehose delivery stream to s3
+resource "aws_kinesis_firehose_delivery_stream" "webserver" {
+  destination = "extended_s3"
+  name        = "webserver"
+
+  extended_s3_configuration {
+    bucket_arn          = aws_s3_bucket.cloudwatch_logs_bucket.arn
+    buffer_interval     = 300
+    buffer_size         = 128
+    compression_format  = "UNCOMPRESSED"
+    error_output_prefix = "errors/"
+    prefix              = ""
+    role_arn            = aws_iam_role.firehose.arn
+    s3_backup_mode      = "Disabled"
+
+    processing_configuration {
+      enabled = false
+    }
+  }
+  server_side_encryption {
+    enabled  = false
+    key_type = "AWS_OWNED_CMK"
+  }
+}

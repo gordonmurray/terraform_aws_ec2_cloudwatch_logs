88e08faa85ef97714cc121fc9d77a699ca7f7f43
Gordon Murray <gordon@gordonmurray.com>
2022-04-05 21:03:12 +0100

    iam roles and policies

---
+++ iam_policies.tf
@@ -0,0 +1,26 @@
+data "aws_region" "current" {}
+
+resource "aws_iam_role_policy" "webserver" {
+  name   = "webserver-policy"
+  role   = aws_iam_role.assume_role.id
+  policy = data.template_file.webserver_policy.rendered
+}
+
+resource "aws_iam_policy" "firehose" {
+  name   = "webserver-firehose"
+  path   = "/service-role/"
+  policy = data.template_file.firehose_service_role_policy.rendered
+}
+
+data "template_file" "firehose_service_role_policy" {
+  template = file("./files/firehose.template")
+  vars = {
+    bucket        = aws_s3_bucket.cloudwatch_logs_bucket.bucket
+    firehose_name = "webserver-firehose"
+    region        = data.aws_region.current.name
+  }
+}
+
+data "template_file" "webserver_policy" {
+  template = file("./files/webserver.template")
+}
---
+++ iam_roles.tf
@@ -0,0 +1,90 @@
+resource "aws_iam_role" "assume_role" {
+  name               = "webserver-role"
+  assume_role_policy = data.template_file.webserver_role.rendered
+}
+
+resource "aws_iam_instance_profile" "webserver" {
+  name = "webserver-instance-proile"
+  role = aws_iam_role.assume_role.name
+}
+
+resource "aws_iam_role" "firehose" {
+  name = "webserver-firehose"
+  path = "/service-role/"
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "firehose.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  force_detach_policies = false
+  managed_policy_arns = [
+    aws_iam_policy.firehose.arn
+  ]
+  max_session_duration = 3600
+}
+
+resource "aws_iam_role" "cloudwatch_firehose" {
+  name = "webserver-cw-firehose"
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "s3.amazonaws.com"
+          }
+        },
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "logs.eu-west-1.amazonaws.com"
+          }
+        },
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "logs.ap-southeast-2.amazonaws.com"
+          }
+        },
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "logs.us-east-1.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  description           = "Allow Cloudwatch subscriptions to deliver firehose streams"
+  force_detach_policies = false
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonKinesisFirehoseFullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+  ]
+  max_session_duration = 3600
+}
+
+
+data "template_file" "webserver_role" {
+  template = file("./files/webserver_role.template")
+}
+
+data "template_file" "firehose_policy" {
+  template = file("./files/firehose_policy.template")
+}
+
